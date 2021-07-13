b8d35ede656a085a8c27218e0fd569e8e1ae4d89
Kanat Yes <yes.kanat@gmail.com>
2021-07-14 00:28:03 +0300

    old reducers removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PostAdd from "./components/postAdd/postAdd";
-// import PostView from "./components/postView/postView";
 import PostCard from "./components/postCard/postCard";
 import "./App.css";
 import { useSelector } from "react-redux";
@@ -13,7 +12,6 @@ const App = () => {
     });
     return (
         <>
-            {/* <PostView className={showPost ? "post--opened show" : "post--opened"} /> */}
             <PostAdd />
             {cards}
         </>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,10 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import postsReducer from "./postsReducer";
-import commentsReducer from "./commentReducer";
-// import currentPostReducer from "./currentPostReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-// import showPostReducer from "./showPostReducer";
 
 const rootReducer = combineReducers({
-    posts: postsReducer,
-    comments: commentsReducer
+    posts: postsReducer
 });
 
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
